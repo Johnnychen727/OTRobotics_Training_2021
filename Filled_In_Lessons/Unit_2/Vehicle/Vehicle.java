bb3e2672f2142eec268f8e3fe11ed8c209b746d4
lily-s-23 <59377732+lily-s-23@users.noreply.github.com>
2021-09-30 14:36:29 -0400

    Added OOP notes

---
+++ Filled_In_Lessons/Unit_2/Vehicle/Vehicle.java
@@ -0,0 +1,31 @@
+package Filled_In_Lessons.Unit_2.Vehicle;
+
+public class Vehicle {
+    public int numWheels;
+    public String colour;
+    public String placeOfProduction;
+    public int speed;
+ 
+    public Vehicle(int numWheels) {
+        this.numWheels = numWheels;
+    }
+    
+    public Vehicle(int numWheels, String colour, String placeOfProduction, int speed) {
+        this.numWheels = numWheels;
+        this.colour = colour;
+        this.placeOfProduction = placeOfProduction;
+        this.speed = speed;
+    }
+    
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void increaseSpeed() {
+        speed++;
+    }
+
+    public void decreaseSpeed() {
+        speed--;
+    }
+}

c3e7bae215c5ace2bd1e51e8f36aef3de0e5aa26
clarrie-w <clarriewang@hotmail.com>
2021-10-31 15:57:45 -0400

    added syntax in comments

--- Unit_1_Notes/appofbasics2notes/forloopsnotes.java
+++ Unit_1_Notes/appofbasics2notes/forloopsnotes.java
@@ -6,15 +6,19 @@ public class forloopsnotes {
         System.out.println("For loop:");
         
         //create a for loop that iterates from 10 to 1 and prints the outcome
-
+        /* for (declaration; check; incrementation) {
+                do stuff
+        }
+        */
 
         System.out.println("While loop:");
         
-        //declare a variable named x witht the value 10
+        //declare a variable named x with the value 10
 
         //Create a while loop that iterates from 10 to 1 and prints every number
-
-
+        /* while (condition) {
+                do stuff
+        } */
         
     }
 }

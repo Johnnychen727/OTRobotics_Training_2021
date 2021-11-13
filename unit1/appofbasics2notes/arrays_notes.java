63ee217e6e10290b920d3f03d80c0b9642ee8738
clarrie-w <clarriewang@hotmail.com>
2021-11-13 14:46:26 -0500

    reverting to previous version

--- unit1/appofbasics2notes/arrays_notes.java
+++ unit1/appofbasics2notes/arrays_notes.java
@@ -6,6 +6,7 @@ public class arrays_notes {
        
         // Define an array named food that contains 4 elements
         
+        // To define all elements in array: Type[] name = {item1, ..., item4};
         
         /* To define elements in array one at a time: 
         Type[] name = new Type[#ofelements];
@@ -13,18 +14,7 @@ public class arrays_notes {
         ...
         name[i] = value;
         */
-
-        /* food[0] = "dumplings";
-        food[1] */
-
-        // To define all elements in array: Type[] name = {item1, ..., item4};
-        String[] food = {"dumplings", "apples", "ice cream", "carrots"};
-
-
         // Use a for loop to iterate over every element in food and print the value of each element
-        for (int i = 0; i < food.length; i++) {
-            System.out.println(food[i]);
-        }
         
     }
 }

f56d5ac17203cb3bb0dda51c89a992bd93d2d232
Johnnychen727 <1chenjoh4@hdsb.ca>
2021-11-22 11:38:30 -0500

    Lesson

--- unit2/Vehicle/Car.java
+++ unit2/Vehicle/Car.java
@@ -1,26 +1,34 @@
 package unit2.Vehicle;
 
 //Create a public class named Car that extends Vehicle class
-//public class Car extends Vehicle{
+public class Car extends Vehicle{
     
     //declare integer variables mileage and distanceThisSession
-    
+    int mileage;
+    int distanceThisSession;
 
     // Create Car method with parameters :numWheels (int), mileage (int), and distanceThisSession
-        
-        //super(numWheels);
-        //this.mileage = mileage;
-        //this.distanceThisSession = distanceThisSession;
-    
+    public Car(int numWheels, int mileage, int distanceThisSession) {
+        super(numWheels);
+        this.mileage = mileage;
+        this.distanceThisSession = distanceThisSession;
+    }
+       
     //Create another Car method with parameters numWheels, colour, placeofProduction, speed, mileage and distanceThisSession
-        //super(numWheels, colour, placeOfProduction, speed);
-        //this.mileage = mileage;
-        //this.distanceThisSession = distanceThisSession;
+    public Car(int numWheels, String colour, String placeOfProduction, int speed, int, mileage, int distanceThisSession) {
+        super(numWheels, colour, placeOfProduction, speed);
+        this.mileage = mileage;
+        this.distanceThisSession = distanceThisSession;
+    }
+       
     
 
     // Create a method to set the mileage
       // setter function used when we want to update values
-        //mileage += distanceThisSession;
+    public void setMileage() {
+        mileage += distanceThisSession;
+    }
+        
     
 
-//}
+}
--- unit2/Vehicle/Vehicle.java
+++ unit2/Vehicle/Vehicle.java
@@ -3,31 +3,47 @@ package unit2.Vehicle;
 //create public class named vehicle
 
     //create public variables number of wheels (integer), colours (string), placeofproduction(string), speed (int)
-    
+    public class Vehicle {
+        public int numWheels;
+        public String colour;
+        public String placeOfProduction;
+        public int speed;
+    }
 
 
 
 
     //create public vehicle method that takes number of wheels variable
-    
+    public Vehicle(int numWheels) {
+        this.numWheels = numWheels;
+    }
 
     //Create method named vehicle that takes arguments, numWheels, colour, placeOfProduction, and speed
-    
-        //this.numWheels = numWheels;
-        //this.colour = colour;
-        //this.placeOfProduction = placeOfProduction;
-        //this.speed = speed;
+    public Vehicle(int numWheels, String colour, String placeOfProduction, int speed) {
+        this.numWheels = numWheels;
+        this.colour = colour;
+        this.placeOfProduction = placeOfProduction;
+        this.speed = speed;
+    }
+        
     
     
     // create method to get speed
+    public int getSpeed() {
+        return speed;
+    }
 
     //create method to increase speed
-
-        //speed++;
+    public void increaseSpeed() {
+        speed++;
+    }
+        
     
 
     //create a method to decrease speed
-        //speed--;
+    public void decreaseSPeed() {
+        speed--;
+    }
     
-
+    
 //}
